0644e688f76904790ce733c95a47f87d3158642d
lucaspenning <50622975+lucaspenning@users.noreply.github.com>
2019-05-29 09:15:46 -0300

    Update ThingSpeak_Sub.py

--- ThingSpeak_Sub.py
+++ ThingSpeak_Sub.py
@@ -15,8 +15,8 @@ randomNum = int.from_bytes(os.urandom(3), 'little')
 myMqttClient = bytes("client"+str(randomNum), 'utf-8')
 
 THINGSPEAK_URL = b"mqtt.thingspeak.com" 
-THINGSPEAK_USER_ID = b'780987'
-THINGSPEAK_MQTT_API_KEY = b'E4233MZVKTX3ZZVY'
+THINGSPEAK_USER_ID = b'ID_CANAL'
+THINGSPEAK_MQTT_API_KEY = b'CHAVE_API_MQTT'
 client = MQTTClient(client_id=myMqttClient, 
                     server=THINGSPEAK_URL, 
                     user=THINGSPEAK_USER_ID, 
@@ -30,8 +30,8 @@ except Exception as e:
     print('could not connect to MQTT server {}{}'.format(type(e).__name__, e))
     sys.exit()
 
-THINGSPEAK_CHANNEL_ID = b'780987'
-THINGSPEAK_CHANNEL_WRITE_API_KEY = b'IO4INWIR5PFI3KNR'
+THINGSPEAK_CHANNEL_ID = b'ID_DO_CANAL'
+THINGSPEAK_CHANNEL_WRITE_API_KEY = b'CHAVE_WRITE_DO_CANAL'
 subscribeTopic = bytes("channels/{:s}/subscribe/fields/field1/{:s}".format(THINGSPEAK_CHANNEL_ID, THINGSPEAK_CHANNEL_READ_API_KEY), 'utf-8')  
 client.subscribe(subscribeTopic)
 
