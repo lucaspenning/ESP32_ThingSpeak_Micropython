a512ba36a8e2c09b4aeb43ef54759aa30394204f
lucaspenning <50622975+lucaspenning@users.noreply.github.com>
2019-06-03 23:58:32 -0300

    Update ThingSpeak_Pub.py

--- ThingSpeak_Pub.py
+++ ThingSpeak_Pub.py
@@ -3,23 +3,32 @@ Wifi.connect()
 import time
 from machine import ADC,Pin
 from umqtt.simple import MQTTClient
+import dht 
 
 adc0=ADC(Pin(35))  
+sensor = dht.DHT11(Pin(14))
 
 SERVER = "mqtt.thingspeak.com"
 client = MQTTClient("umqtt_client", SERVER)
 
-CHANNEL_ID = "ID_DO_CANAL"
-WRITE_API_KEY = "CHAVE_WRITE_DO_CANAL"
+CHANNEL_ID = "780987"
+WRITE_API_KEY = "IO4INWIR5PFI3KNR"
 
 topic = "channels/" + CHANNEL_ID + "/publish/" + WRITE_API_KEY
 
+temp = 0
+valorPublicado = 0
+
 while True:
-    temp = adc0.read()/50 
-    payload = "field1="+str(temp)
+    sensor.measure()
+    temperatura = sensor.temperature()
+    print(temperatura)
+    umidade = sensor.humidity()
+    print(umidade)
+    luminosidade = adc0.read()/40.95 
+    print(luminosidade)
+    payload = "field1="+str(luminosidade) + "&field2="+str(temperatura) + "&field3="+str(umidade)
     client.connect()
     client.publish(topic, payload)
     client.disconnect()
     time.sleep(20)
-
-
